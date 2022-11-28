9a8ebb612bd8cbdee186ba54f82f80e54deb4b05
László Stahorszki <laszlo.stahorszki@ropi.dev>
2022-11-28 21:25:35 +0100

    integrate suffix input

--- bump-version.sh
+++ bump-version.sh
@@ -1,7 +1,7 @@
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 # shellcheck disable=SC2001
-eval "$(echo "$VERSION" | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)\(.*\)$/MAJOR=\1\nMINOR=\2\nPATCH=\3\nSUFFIX=\4/')"
+eval "$(echo "$VERSION" | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)\(.*\)$/MAJOR=\1\nMINOR=\2\nPATCH=\3\nORIGINAL_SUFFIX=\4/')"
 PATCH=$((PATCH+1))
 
 echo "$MAJOR.$MINOR.$PATCH$SUFFIX"
