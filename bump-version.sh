76d835c0ccc606d5bbbbfb5f459af4deeda62cba
László Stahorszki <laszlo.stahorszki@ropi.dev>
2023-01-05 23:09:32 +0100

    Add ability to set a version to derive next version from

--- bump-version.sh
+++ bump-version.sh
@@ -1,4 +1,4 @@
-VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+VERSION=${INPUT_PREVIOUSVERSION:-$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)}
 
 # shellcheck disable=SC2001
 eval "$(echo "$VERSION" | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)\(.*\)$/MAJOR=\1\nMINOR=\2\nPATCH=\3\nORIGINAL_SUFFIX=\4/')"
